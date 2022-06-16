36abc16b0c46a314d942298d7728bbdf2e5e8a57
zdd <zhangw@suparking.cn>
2022-06-16 13:40:31 +0800

    合约管理相关接口

--- shared-trad-order-serv/shared-trad-order-impl/src/main/java/cn/suparking/order/service/impl/CarGroupOrderServiceImpl.java
+++ shared-trad-order-serv/shared-trad-order-impl/src/main/java/cn/suparking/order/service/impl/CarGroupOrderServiceImpl.java
@@ -1,9 +1,9 @@
 package cn.suparking.order.service.impl;
 
 import api.beans.CarGroupOrderDTO;
-import cn.suparking.order.service.CarGroupOrderService;
 import cn.suparking.order.entity.CarGroupOrderDO;
 import cn.suparking.order.mapper.CarGroupOrderMapper;
+import cn.suparking.order.service.CarGroupOrderService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
